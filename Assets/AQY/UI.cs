7e110b7f2bd27166bcd27ada31e60193f4332599
JunLangBai <1279477694@qq.com>
2025-04-22 22:11:21 +0800

    UI

---
+++ Assets/AQY/UI.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI : MonoBehaviour
+{
+    public Camera mainCamera; // 主摄像机
+    public GameObject a;
+    // Update is called once per frame
+    void Update()
+    {
+        // 计算从UI图片到目标物体的方向
+        Vector3 direction = a.transform.position - transform.position;
+
+        // 使UI图片的正面朝向目标物体
+        transform.rotation = Quaternion.LookRotation(direction, Vector3.up);
+    }
+}
