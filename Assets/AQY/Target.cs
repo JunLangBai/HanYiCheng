ff165c33677944738e5f23ec99300865017e8a10
JunLangBai <1279477694@qq.com>
2025-04-26 00:05:43 +0800

    0DOF

--- Assets/AQY/Target.cs
+++ Assets/AQY/Target.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
-    public GameObject a;
+    public GameObject a; // 目标物体
+    public Vector3 offset; // 当前物体相对于目标物体的偏移量
+
     // Update is called once per frame
     void Update()
     {
@@ -13,5 +15,8 @@ public class Target : MonoBehaviour
 
         // 使UI图片的正面朝向目标物体
         transform.rotation = Quaternion.LookRotation(-direction, Vector3.up);
+
+        // 设置当前物体的位置为a物体的位置加上偏移量
+        transform.position = a.transform.position + offset;
     }
 }
