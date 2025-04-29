74f58a84289cb4ca7651e27d0186a38fbfb4df3b
JunLangBai <1279477694@qq.com>
2025-04-29 21:49:48 +0800

    Json序列化关卡

--- Assets/AZ/LevelSection/LevelSelectMngr.cs
+++ Assets/AZ/LevelSection/LevelSelectMngr.cs
@@ -102,4 +102,31 @@ public class LevelSelectMngr : MonoBehaviour
          levelButton.Setup(CurrentArea.Levels[i], UnlockedLevelIDs.Contains(CurrentArea.Levels[i].LevelID));
       }
    }
+   
+   //通关后标记为true
+   // public void LevelSelected()
+   // {
+   //    GameData gameData = JsonFileManager.LoadFromJson<GameData>("GameData.json");
+   //
+   //    if (gameData != null)
+   //    {
+   //       // 找到目标 LevelDataJson 对象
+   //       var targetLevel = gameData.levels.FirstOrDefault(l => l.Scene == SceneManager.GetActiveScene().name);
+   //
+   //       if (targetLevel != null)
+   //       {
+   //          // 修改 ISUnlockedByDefault 值
+   //          targetLevel.ISUnlockedByDefault = true;
+   //
+   //          Debug.Log($"Updated LevelID: {targetLevel.Scene}, ISUnlockedByDefault: {targetLevel.ISUnlockedByDefault}");
+   //
+   //          // 保存修改后的数据回 JSON 文件
+   //          JsonFileManager.SaveToJson(gameData, "GameData.json");
+   //       }
+   //       else
+   //       {
+   //          Debug.LogWarning("Level with ID '关卡数据盒名字' not found!");
+   //       }
+   //    }
+   // }
 }
