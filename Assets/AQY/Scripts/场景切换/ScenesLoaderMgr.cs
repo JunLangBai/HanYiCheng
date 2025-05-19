97fdf8fd004592e37e761b8e21c9af7056f4d2f6
JunLangBai <1279477694@qq.com>
2025-05-19 22:34:00 +0800

    场景切换

--- Assets/AQY/Scripts/场景切换/ScenesLoaderMgr.cs
+++ Assets/AQY/Scripts/场景切换/ScenesLoaderMgr.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using  UnityEngine.SceneManagement;
 
-public class BackSceneMgr : MonoBehaviour
+public class ScenesLoaderMgr : MonoBehaviour
 {
     public string SceneName;
 
