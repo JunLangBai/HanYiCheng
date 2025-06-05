3a37a3b3b97d230109ab30b5a92d4005e4b96d63
JunLangBai <1279477694@qq.com>
2025-06-05 21:51:33 +0800

    入场

---
+++ Assets/AQY/Scripts/图片跳动/ImagePopup.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class BounceFadeInMoveSimultaneous : MonoBehaviour
+{
+    [Header("起点和目标")]
+    public RectTransform startRect;
+    public RectTransform targetRect;
+
+    [Header("组件引用")]
+    public RectTransform imageRect;
+    public CanvasGroup canvasGroup;
+
+    [Header("动画设置")]
+    public float fadeInDuration = 0.5f;
+    public float moveDuration = 1f;
+    public bool disableAfter = false;
+
+    void Start()
+    {
+        // 初始化位置和透明度
+        imageRect.anchoredPosition = startRect.anchoredPosition;
+        canvasGroup.alpha = 0f;
+
+        // 同时启动淡入与移动动画
+        canvasGroup.DOFade(1f, fadeInDuration);
+        imageRect.DOAnchorPos(targetRect.anchoredPosition, moveDuration)
+            .SetEase(Ease.OutBounce)
+            .OnComplete(() => {
+                if (disableAfter)
+                    gameObject.SetActive(false);
+            });
+    }
+}
--- Assets/AQY/Scripts/进入场景/InitialLoading.cs
+++ Assets/AQY/Scripts/进入场景/InitialLoading.cs
@@ -3,14 +3,47 @@ using UnityEngine.SceneManagement;
 
 public class InitialLoading : MonoBehaviour
 {
+    public float time;
+    public float musicTime;
+    public AudioSource audio;
+    
     private void Start()
     {
+        Invoke("SoundLoading",musicTime);
+        
         if (JsonManager.Instance.gameData.placementClear == false &&
             JsonManager.Instance.gameData.tutorialClear == false)
-            SceneManager.LoadScene("PlacementUI");
-        else if (JsonManager.Instance.gameData.placementClear && JsonManager.Instance.gameData.tutorialClear == false)
-            SceneManager.LoadScene("Tutorial");
+        {
+            Invoke("LoadPlacementUI", time);
+        }
+        else if (JsonManager.Instance.gameData.placementClear && 
+                 JsonManager.Instance.gameData.tutorialClear == false)
+        {
+            Invoke("LoadTutorial", time);
+        }
         else
-            SceneManager.LoadScene("MainUI");
+        {
+            Invoke("LoadMainUI", time);
+        }
+    }
+
+    private void SoundLoading()
+    {
+        audio.Play();
+    }
+
+    private void LoadPlacementUI()
+    {
+        SceneManager.LoadScene("PlacementUI");
+    }
+
+    private void LoadTutorial()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+
+    private void LoadMainUI()
+    {
+        SceneManager.LoadScene("MainUI");
     }
 }
